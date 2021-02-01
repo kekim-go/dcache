a211a06f43b863bee16db2d38361a37027bfdcf8
cloud@cloud-server-181 <cloud@cloud-server-181.none>
2021-02-01 11:43:16 +0900

    file list를 map-list 구조로 변경하여 sort속도 개선

--- bin/purge.dart
+++ bin/purge.dart
@@ -105,6 +105,7 @@ class Purge {
               return succeed;
             const bool recursive = false;
             const bool followLinks = false;
+            var elapsed = Map();
 
             Stopwatch watch = Stopwatch();
             watch.start();
@@ -113,11 +114,25 @@ class Purge {
               followLinks: followLinks,
             );
             watch.stop();
-            final int elapsedFileList = watch.elapsedMilliseconds;
-            int elapsedFileSort = 0;
-            int elapsedFileDelete = 0;
-
-
+            elapsed["FileList"] = watch.elapsedMilliseconds;
+            
+            watch.reset();
+            watch.start();
+            Map<int, List<File>> fileMap = Map();
+            for (var f in files) {
+              int modified = (f as File).lastModifiedSync().millisecondsSinceEpoch;
+              if (!fileMap.containsKey(modified)) {
+                fileMap[modified] = List<File>();
+              }
+              fileMap[modified].add(f as File);
+            }
+            watch.stop();
+            elapsed["MakeMap"] = watch.elapsedMilliseconds;
+            print('fileMap len=${fileMap.length}');
+            // for (var key in fileMap.keys) {
+            //   print('key=$key, count=${fileMap[key].length}');
+            // }
+            
             const bool purgeReally = true;
             final bool purgeHere = files.length > count;
             if (printAllFiles) {
@@ -127,32 +142,59 @@ class Purge {
                 print('file[$i]=$file');
               }
             }
+            
             if (purgeHere) {
               // print('> too many files in a path: path=$found, files=${files.length}, count=$count');
               numFiles = files.length;
 
-              watch.reset();
-              watch.start();
-              files.sort((a, b) {
-                final int l = (a as File).lastModifiedSync().millisecondsSinceEpoch;
-                final int r = (b as File).lastModifiedSync().millisecondsSinceEpoch;
-                return r.compareTo(l);
-              });
-              watch.stop();
-              elapsedFileSort = watch.elapsedMilliseconds;
+              // watch.reset();
+              // watch.start();
+              // files.sort((a, b) {
+              //   final int l = (a as File).lastModifiedSync().millisecondsSinceEpoch;
+              //   final int r = (b as File).lastModifiedSync().millisecondsSinceEpoch;
+              //   return r.compareTo(l);
+              // });
+              // watch.stop();
+              // elapsed["FileSort"] = watch.elapsedMilliseconds;
+
+              // watch.reset();
+              // watch.start();
+              // for (int i=count; i<files.length; i++) {
+              //   final String file = files[i].path;
+              //   final DateTime datetime = lastModified(file);
+              //   // print('>>> deleted: index=$i, file=$file, datetime=$datetime');
+              //   if (purgeReally) delete(file);
+              // }
+              // watch.stop();
+              // elapsed["FileDelete"] = watch.elapsedMilliseconds;
 
               watch.reset();
               watch.start();
-              for (int i=count; i<files.length; i++) {
-                final String file = files[i].path;
-                final DateTime datetime = lastModified(file);
-                // print('>>> deleted: index=$i, file=$file, datetime=$datetime');
-                if (purgeReally) delete(file);
+              var sortedKeys = fileMap.keys.toList()..sort();
+              watch.stop();
+              elapsed["KeySorted"] = watch.elapsedMilliseconds;
+            
+              watch.reset();
+              watch.start();
+              int fileCounts = 0;
+              for (var key in sortedKeys) {
+                if ((fileCounts + fileMap[key].length) > count) {
+                  // print('>>> deleting : key=${key}, length=${fileMap[key].length}, fileCounts=$fileCounts');
+                  for (File f in fileMap[key]) {
+                    final String file = f.path;
+                    // print('>>> deleted: key=$key, file=$file');
+                    if (purgeReally) delete(file);
+                  }
+                } else {
+                  fileCounts += fileMap[key].length;
+                  // print('>>> adding   : key=${key}, length=${fileMap[key].length}, fileCounts=$fileCounts');
+                }
               }
               watch.stop();
-              elapsedFileDelete = watch.elapsedMilliseconds;
+              elapsed["FileDelete"] = watch.elapsedMilliseconds;
+
             }
-            print('$numFiles,$elapsedFileList,$elapsedFileSort,$elapsedFileDelete,${elapsedFileList+elapsedFileSort+elapsedFileDelete}');
+            print('$numFiles,${elapsed["FileList"]},${elapsed["MakeMap"]},${elapsed["KeySorted"]},${elapsed["FileDelete"]}');//,${elapsedFileList+elapsedFileSort+elapsedFileDelete}
             succeed = true;
           }
           catch (exc) {
--- dcache/gen_files.sh
+++ dcache/gen_files.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "usage: ./gen_files.sh start-number end-number folder-name"
+echo "./gen_files.sh 1 1000 a"
+echo " "
+
 for ((i=$1;i<=$2;i++))
 do
         touch ./cache/$3/$i.test
