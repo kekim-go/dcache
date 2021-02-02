60a27c555a81fc55cb7b54a1368ee05ae2e07b65
cloud@cloud-server-181 <cloud@cloud-server-181.none>
2021-02-02 12:04:10 +0900

    Modified가 옛날 꺼부터 삭제하도록 수정 로그 수정

--- bin/purge.dart
+++ bin/purge.dart
@@ -79,10 +79,10 @@ class Purge {
       print('$function: $exc');
     }
     finally {
-      final bool printAllFiles = true;//printAll.parseBool();
+      final bool printAllFiles = printAll.parseBool();
       if (printAllFiles) {
         final int consumed = _consume.elapsedMilliseconds;
-        // print('purge: purged=$purged, consumed=$consumed <- root=$root, count=$count, printAll=$printAll');
+        print('purge: purged=$purged, consumed=$consumed <- root=$root, count=$count, printAll=$printAll');
       }
       _consume.reset();
     }
@@ -113,88 +113,68 @@ class Purge {
               recursive: recursive,
               followLinks: followLinks,
             );
+            numFiles = files.length;
             watch.stop();
             elapsed["FileList"] = watch.elapsedMilliseconds;
             
             watch.reset();
             watch.start();
-            Map<int, List<File>> fileMap = Map();
+            Map<int, List<File>> mapFiles = Map();
             for (var f in files) {
               int modified = (f as File).lastModifiedSync().millisecondsSinceEpoch;
-              if (!fileMap.containsKey(modified)) {
-                fileMap[modified] = List<File>();
+              if (!mapFiles.containsKey(modified)) {
+                mapFiles[modified] = List<File>();
               }
-              fileMap[modified].add(f as File);
+              mapFiles[modified].add(f as File);
             }
+            var sortedKeys = mapFiles.keys.toList()..sort();
             watch.stop();
-            elapsed["MakeMap"] = watch.elapsedMilliseconds;
-            print('fileMap len=${fileMap.length}');
-            // for (var key in fileMap.keys) {
-            //   print('key=$key, count=${fileMap[key].length}');
-            // }
+            elapsed["KeySorted"] = watch.elapsedMilliseconds;
             
             const bool purgeReally = true;
             final bool purgeHere = files.length > count;
             if (printAllFiles) {
-              print('> path=$found: files=${files.length}');
+              print('> path=$found: fileMap len=${mapFiles.length}, files=${files.length}');
+              for (var key in sortedKeys) {
+                print('key=$key, count=${mapFiles[key].length}');
+                for (File f in mapFiles[key]) {
+                  final String file = f.path;
+                  print('key=$key, file=$file');
+                }
+              }
               for (int i=0; i<files.length; i++) {
-                final String file = files[i].path;
-                print('file[$i]=$file');
               }
             }
             
             if (purgeHere) {
-              // print('> too many files in a path: path=$found, files=${files.length}, count=$count');
-              numFiles = files.length;
-
-              // watch.reset();
-              // watch.start();
-              // files.sort((a, b) {
-              //   final int l = (a as File).lastModifiedSync().millisecondsSinceEpoch;
-              //   final int r = (b as File).lastModifiedSync().millisecondsSinceEpoch;
-              //   return r.compareTo(l);
-              // });
-              // watch.stop();
-              // elapsed["FileSort"] = watch.elapsedMilliseconds;
-
-              // watch.reset();
-              // watch.start();
-              // for (int i=count; i<files.length; i++) {
-              //   final String file = files[i].path;
-              //   final DateTime datetime = lastModified(file);
-              //   // print('>>> deleted: index=$i, file=$file, datetime=$datetime');
-              //   if (purgeReally) delete(file);
-              // }
-              // watch.stop();
-              // elapsed["FileDelete"] = watch.elapsedMilliseconds;
-
+              print('> too many files in a path: path=$found, files=${numFiles}, count=$count');
               watch.reset();
               watch.start();
-              var sortedKeys = fileMap.keys.toList()..sort();
-              watch.stop();
-              elapsed["KeySorted"] = watch.elapsedMilliseconds;
-            
-              watch.reset();
-              watch.start();
-              int fileCounts = 0;
               for (var key in sortedKeys) {
-                if ((fileCounts + fileMap[key].length) > count) {
-                  // print('>>> deleting : key=${key}, length=${fileMap[key].length}, fileCounts=$fileCounts');
-                  for (File f in fileMap[key]) {
+                if (numFiles > count) {
+                  for (File f in mapFiles[key]) {
                     final String file = f.path;
-                    // print('>>> deleted: key=$key, file=$file');
-                    if (purgeReally) delete(file);
+                    if (purgeReally && (numFiles > count)) {
+                      delete(file);
+                      numFiles--;
+                    } else {
+                      break;
+                    }
                   }
+                  // print('>>> deleting : key=${key}, length=${mapFiles[key].length}, numFiles=$numFiles > ${numFiles-mapFiles[key].length}');
                 } else {
-                  fileCounts += fileMap[key].length;
-                  // print('>>> adding   : key=${key}, length=${fileMap[key].length}, fileCounts=$fileCounts');
+                  break;
                 }
               }
               watch.stop();
               elapsed["FileDelete"] = watch.elapsedMilliseconds;
-
             }
-            print('$numFiles,${elapsed["FileList"]},${elapsed["MakeMap"]},${elapsed["KeySorted"]},${elapsed["FileDelete"]}');//,${elapsedFileList+elapsedFileSort+elapsedFileDelete}
+            
+            int total = 0;
+            for (var key in elapsed.keys) {
+              total += elapsed[key];
+            }
+            print('$numFiles,${elapsed["FileList"]},${elapsed["KeySorted"]},${elapsed["FileDelete"]},$total');
             succeed = true;
           }
           catch (exc) {
